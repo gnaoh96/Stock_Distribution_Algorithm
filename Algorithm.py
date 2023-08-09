e1d97f9583eb3bc0fdd49d4aa39e8b6c6f371cd1
HoangPhamRepo <hoangpham.data@gmail.com>
2023-08-09 09:13:38 +0700

    *20230807

--- Algorithm.py
+++ Algorithm.py
@@ -77,10 +77,173 @@ def cal_balance_num(state, stock, power):
 
 # Function to allot goods
 def allot(df_1, df_2):
-    if df_2['Area'] == "HồChíMinh" or df_2['Area'] == "HàNội":
-        pool_1_transfer = sort_by_balance(df_2[df_2['Statement']=='residual'])
-        pool_2_get = sort_by_balance(df_2[df_2['Statement']=='deficient'])
-        pritn(pool_1_transfer)
+    for df in df_2:
+        # create copy dataframe
+        data = df.copy()
+        # Initialize pool_1(transfer), pool_2(recevice)
+        pool_1 = sort_by_balance(data[data['Statement']=='residual'])
+        pool_2 = sort_by_balance(data[data['Statement']=='deficient'])
+        pool_1_to_HO = pd.DataFrame()
+        pool_2_remain = pd.DataFrame()
+
+        if list(data.iterrows())[0][1]['Area'] == "HồChíMinh" or list(data.iterrows())[0][1]['Area'] == "HàNội":
+            pool_1_clone = pool_1
+            pool_2_clone = pool_2
+
+            # Initialize cache_transaction (dataframe store productid to transfer - max transfer to min receive)
+            cache_trans = pool_2_clone[pool_2_clone['productId'] == pool_1_clone.iloc[0]['productId']]
+
+            ## Rule: Compare the productId and Balance number
+            # Initialize transactions dataframe to store logs of internal transactions
+            transactions = pd.DataFrame(colums = ['storeId_transfer','storeName_transfer', 'storeId_receive'
+                                                    ,'storeName_receive','productId','productName','Quantity'])
+
+            # Initialize pool_3 dataframe to store residual productId and no need to receive => Move to HO
+            pool_3 = pd.DataFrame(columns = ['storeId_HO','storeName_HO', 'productId','productName','Quantity'])
+
+            # start while loop to distribute goods
+            while True:
+                # Initialize amount of residual goods in pool_1:
+                n = pool_1_clone.iloc[0]['Balance_num']
+
+                # resolve issue when cache_transaction is blank (no need to receive => move goods to HO), create instance dict to store residual goods and concat to pool_3
+                if len(cache_trans.index) == 0  :
+                    residual_dict = {'storeId_HO': pool_1_transfer_clone.iloc[0]['storeId'],
+                                    'storeName_HO': pool_1_transfer_clone.iloc[0]['storeName'],
+                                    'productId': pool_1_transfer_clone.iloc[0]['productId'],
+                                    'productName': pool_1_transfer_clone.iloc[0]['productName'],
+                                    'Quantity': n}
+
+                    pool_3 = pd.concat([pool_3, pd.DataFrame(data = residual_dict, index=[0])], ignore_index= False)
+
+                    # drop residual goods in pool_1_clone:
+                    pool_1_clone.drop(index= pool_1_clone.iloc[0:1,:].index, inplace = True)
+
+                    # break loop if len(pool_1_clone) = 0 - when residual goods is over
+                    if len(pool_1_clone.index) == 0:
+                        break
+
+                    # update cache_trans with new productId at first index of pool_1_clone
+                    cache_trans = pool_2_clone[pool_2_clone['productId'] == pool_1_clone.iloc[0]['productId']]
+                    # continue loop outer if
+                    continue
+
+                # Initialize amount of deficient goods in cache_trans:
+                m = cache_trans.iloc[0]['Balance_num']
+
+                ## Case 1: n > m; transfer > receive
+                if n > m:
+                    # Initialize amount of residual goods remain
+                    remain_amount = n - m
+
+                    # Ininitalize dict to write log of transaction
+                    translog_dict = {'storeId_transfer': pool_1_transfer_clone.iloc[0]['storeId'],
+                                     'storeName_transfer': pool_1_transfer_clone.iloc[0]['storeName'],
+                                     'storeId_receive':    cache_transaction.iloc[0]['storeId'],
+                                     'storeName_receive':  cache_transaction.iloc[0]['storeName'],
+                                     'productId':          pool_1_transfer_clone.iloc[0]['productId'],
+                                     'productName':        pool_1_transfer_clone.iloc[0]['productName'],'Quantity': m}
+                    # concat to transactions dataframe
+                    transactions = pd.concat([transactions, pd.DataFrame(data = translog_dict, index= [0])], ignore_index= False)
+
+                    # update balance_num in 2 pools
+                    # update pool_1_clone
+                    pool_1_clone.loc[pool_1_clone.index[0], ['Balance_num']] = remain_amount
+                    # update pool_2_clone
+                    pool_2_clone.drop(index= cache_trans.iloc[0:1,:].index, inplace= True)
+
+                    # re-sorting pool to find out first index goods
+                    pool_1_clone = sort_by_balance(pool_1_clone)
+                    pool_2_clone = sort_by_balance(pool_2_clone)
+
+                    # BREAK: When pool_2 - deficient goods is over, no need to receive more
+                    if len(pool_2_clone.index) == 0:
+                        break
+
+                    # If loop is exist, then update cache_trans
+                    cache_trans = pool_2_clone[pool_2_clone['productId'] == pool_1_clone.iloc[0]['productId']]
+
+                    # Continue to the next loop
+                    continue
+
+                ## Case 2: m > n and n != 0 => receive > transfer
+                elif m > n and (n != 0 or n != None):
+                    remain_amount = m - n
+
+                    # Intialize dict to store log of transaction
+                    translog_dict = {'storeId_transfer': pool_1_transfer_clone.iloc[0]['storeId'],
+                                     'storeName_transfer': pool_1_transfer_clone.iloc[0]['storeName'],
+                                     'storeId_receive':    cache_transaction.iloc[0]['storeId'],
+                                     'storeName_receive':  cache_transaction.iloc[0]['storeName'],
+                                     'productId':          pool_1_transfer_clone.iloc[0]['productId'],
+                                     'productName':        pool_1_transfer_clone.iloc[0]['productName'], 'Quantity': n}
+                    # Add logs to transactions dataframe
+                    transactions = pd.concat([transactions, pd.DataFrame(data= translog_dict, index=[0])], ignore_index= False)
+
+                    # update balance_num in pool_2_clone
+                    pool_2_clone.loc[cache_trans.iloc[0].index,['Balance_num']] = remain_amount
+
+                    # update stock in pool_2_clone (to run Round 2: HO to Areas) ***
+                    pool_2_clone.loc[cache_trans.iloc[0].index,['StockQuantity']] = pool_2_clone.loc[cache_trans.iloc[0].index,['StockQuantity']] + n
+
+                    # drop goods is over in pool_1
+                    pool_1_clone.drop(index = pool_1_clone.iloc[0:1, :].index, inplace = True)
+
+                    # Re-sorting pool
+                    pool_1_clone = sort_by_balance(pool_1_clone)
+                    pool_2_clone = sort_by_balance(pool_2_clone)
+
+                    # BREAK when goods in pool_1_clone is over
+                    if len(pool_1_clone.index)==0:
+                        break
+
+                    # Update cache_trans and continue loop
+                    cache_trans = pool_2_clone[pool_2_clone['productId']==pool_1_clone.iloc[0]['productId']]
+                    continue
+
+                ## Case 3: Receive > Transfer (m > n) (1 - 0 = 1, case when n = 0 or n = None)
+                elif m > n and (n == 0 or n == None):
+                    # remain_amount = 0
+                    # m does not have chane => don't need to update pool_2_clone
+
+                    # drop n in pool_1_clone
+                    pool_1_clone.drop(index=pool_1_clone.iloc[0:1,:].index ,inplace=True)
+
+                    # Re-sorting pool
+                    pool_1_clone = sort_by_balance(pool_1_clone)
+                    pool_2_clone = sort_by_balance(pool_2_clone)
+
+                    # BREAK when pool_1_clone is over
+                    if len(pool_1_clone.index) == 0:
+                        break
+
+                    # Update cache_trans and continue loop
+                    cache_trans = pool_2_clone[pool_2_clone['productId'] == pool_1_clone.iloc[0]['productId']]
+                    continue
+
+                ## Case 4: Nothing
+                else: None
+
+            # Filter transactions DataFrame:
+            transactions = transactions[transactions['Quantity'] > 0]
+
+            # Filter goods to HO with quantity > 0:
+            pool_3 = pool_3[pool_3['Quantity']>0]
+
+            # define excel filepath
+            out_path = f"{path}{data.iloc[0]['Area']}.xlsx" # split Areas HCM & HN
+            writer = pd.ExcelWriter(out_path , engine = 'xlsxwriter')
+
+            # export excel file store transactions
+            transactions.to_excel(writer, sheet_name=f"trans_{data.iloc[0]['Area']}", encoding= 'utf-8-sig', index= False)
+
+            # export excel file residual goods to HO
+            pool_3.to_excel(writer, sheet_name=f"{data.iloc[0]['Area']}_transHO", encoding="utf-8-sig", index= False)
+            writer.save()
+
+            #
+
+
 
 
 # define df_2 copy of df (main stocks's df)
@@ -106,5 +269,6 @@ df_2 = [df_2[df_2.Area == list_area_2[area]] for area in range(0,len(list_area_2
 #pool_2 = sort_by_balance(df_2[df_2['Statement']=='deficient'])
 
 
+allot(df_1, df_2)
 
 
